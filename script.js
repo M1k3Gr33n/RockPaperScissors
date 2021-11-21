2b1c4e3274bac34eb52457dff5f83d559d145fec
michael <michaelldangreen91@gmail.com>
2021-11-20 21:36:56 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // creating a rock paper scissors game
 
 const moves = ["Rock","Paper","Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 function computerPlay(){
   return moves[Math.floor(Math.random() * 3)];
 }
@@ -8,35 +10,28 @@ computerPlay()
 
 function playRound(playerSelection, computerSelection) {
   // return a string stating who won the round ie "You lost! Paper beats Rock!"
-  if (playerSelection == 'rock' && computerSelection == 'Scissors') {
+  if (playerSelection == 'rock' && computerSelection == 'Scissors' || playerSelection == 'paper' && computerSelection == 'Rock') {
+    playerScore = playerScore + 1;
     return 'You win! You had: ' + playerSelection + ' beats ' + computerSelection.toLowerCase();
   } 
-  else if (playerSelection == 'rock' && computerSelection == 'Paper') {
-    return 'You lose. Computer had: ' + computerSelection.toLowerCase() + ' beats ' + playerSelection;
-  } 
-  else if (playerSelection == 'PAPER' && computerSelection == 'Rock') {
-    return 'You win! You had: ' + playerSelection + ' beats ' + computerSelection.toLowerCase();
-  } 
-  else if (playerSelection == 'PAPER' && computerSelection == 'Scissors') {
-    return 'You lose. Computer had: ' + computerSelection.toLowerCase() + ' beats ' + playerSelection;
-  } 
-  else if (playerSelection == 'SCISSORS' && computerSelection == 'Paper') {
-    return 'You win! You had: ' + playerSelection + ' beats ' + computerSelection.toLowerCase();
-  } 
-  else if (playerSelection == 'SCISSORS' && computerSelection == 'Rock') {
+  else if (playerSelection == 'rock' && computerSelection == 'Paper' || playerSelection == 'paper' && computerSelection == 'Scissors' || playerSelection == 'scissors' && computerSelection == 'Rock') {
+    computerScore = computerScore + 1;
     return 'You lose. Computer had: ' + computerSelection.toLowerCase() + ' beats ' + playerSelection;
   } 
   else (playerSelection.toLowerCase() == computerSelection) 
-    return 'You tied. ' + playerSelection + ' is the same as ' + computerSelection.toLowerCase();
-  
+    return 'You tied. ' + playerSelection + ' is the same as ' + computerSelection.toLowerCase(); 
 } 
-const playerSelection = prompt("Rock, Paper, or Scissors? ").toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-  playRound(playerSelection,computerSelection);
-  return 'The winner of this round is';
+  const playerSelection = prompt("Rock, Paper, or Scissors? ").toLowerCase();
+  const computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection);
+      return 'You had ' + playerSelection + ' and the computer had ' + computerSelection + '.'
+      + 'So the current score is Player: ' + playerScore + ' and Computer: ' + computerScore + '.'  
 }
 
-console.log(game());
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
+console.log(game())
